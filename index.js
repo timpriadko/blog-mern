8c1e76827c0587e7c3829397d9b6aa0097d0d026
timpriadko <timpriadko@gmail.com>
2024-05-02 15:08:19 +0300

    added login, getMe endpoints, UserController

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
 
-import UserModel from "./models/User.js";
+import checkAuth from "./utils/checkAuth.js";
+
+import * as UserController from "./controllers/UserController.js";
 
 mongoose
 	.connect(
@@ -19,46 +18,9 @@ const app = express();
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello World!');
-// });
-
-// app.post('/auth/login', (req, res) => {
-// 	// todo - rm
-// 	console.log(req.body)
-
-// 	const token = jwt.sign({
-// 		email: req.body.email,
-// 		fullName: 'John Doe'
-// 	}, 'secret123');
-
-// 	res.json({
-// 		success: true,
-// 		token
-// 	})
-// })
-
-app.post("/auth/register", registerValidation, async (req, res) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(400).json(errors.array());
-	}
-
-	const password = req.body.password;
-	const salt = await bcrypt.genSalt(10);
-	const passwordHash = await bcrypt.hash(password, salt);
-
-	const doc = new UserModel({
-		email: req.body.email,
-		fullName: req.body.fullName,
-		avatarUrl: req.body.avatarUrl,
-		passwordHash,
-	});
-
-	const user = await doc.save();
-
-	res.json(user);
-});
+app.post("/auth/login", UserController.login);
+app.post("/auth/register", registerValidation, UserController.register);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
 	if (err) {
